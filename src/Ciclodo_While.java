0118505531dcc976faefd1553f0354b5db5a842a
Ocatvio Caballero <Octaandres07@gmail.com>
2026-09-17 10:38:04 -0300

    agregado el archivo ciclo do while

--- src/Ciclodo_While.java
+++ src/Ciclodo_While.java
@@ -1,2 +1,10 @@
 public class Ciclodo_While {
+    public static void main(String[] args) {
+        System.out.println("***Ciclo do-while***");
+
+        int contador = 10;
+        do {
+            System.out.println(contador--);
+        } while (contador >= 1);
+    }
 }
